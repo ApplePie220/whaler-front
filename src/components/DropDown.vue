ac32fc2553cd9fee41a571005fd0ac6a49a22461
KreespK <darkmilk85@gmail.com>
2024-04-25 23:57:02 +0300

    Добавила silly анимацию, нико не поймет, куда

--- src/components/DropDown.vue
+++ src/components/DropDown.vue
@@ -3,8 +3,8 @@
     <a href="#">
       {{ title }}
     </a>
-    <svg viewBox="0 0 1030 638" width="10">
-      <path d="M1017 68L541 626q-11 12-26 12t-26-12L13 68Q-3 49 6 24.5T39 0h952q24 0 33 24.5t-7 43.5z" fill="#FFF"></path>
+    <svg viewBox="0 0 1030 638" width="10" :class="{'open': isOpen}">
+      <path d="M1017 68L541 626q-11 12-26 12t-26-12L13 68Q-3 49 6 24.5T39 0h952q24 0 33 24.5t-7 43.5z" fill="#660000"></path>
     </svg>
     <transition name="fade" appear>
       <div class="sub-menu" v-if="isOpen">
@@ -43,6 +43,21 @@ nav .menu-item .sub-menu {
   width: max-content;
   border-radius: 0px 0px 16px 16px;
   z-index: 1000; /* Add this line */
+}
+
+nav .menu-item:hover {
+  /* add styles here, e.g. */
+  background-color: #c8bcbc; /* light gray background on hover */
+  cursor: pointer; /* change cursor to pointer on hover */
+}
+
+nav .menu-item svg {
+  width: 10px;
+  margin-left: 10px;
+}
+
+svg.open {
+  transform: rotate(180deg);
 }
 
 .fade-enter-active,
